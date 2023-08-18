2c124c70894653e1b54404da47241c2ed1208d25
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-08-18 14:39:11 +0500

    edit files

--- scripts/metadata.py
+++ scripts/metadata.py
@@ -1,6 +1,4 @@
-import os
-import requests  
-import json
+import os, requests, json
 from brownie import PromissoryNFT, Promissory
 from dotenv import load_dotenv
 
@@ -57,14 +55,12 @@ metadata_template = {
     }
 
 def create_metadata(_from, token_id):
-    # кол-во выпущеных токенов
     print(f'Token id: {token_id}')
     # копируем шаблон метаданных
     collectible_metadata = metadata_template.copy()
     # имя токена = его id
     collectible_metadata["name"] = f"Cypto-promise №{str(token_id)}"
 
-    # получаем инфу о контракте
     promissory_info = get_promissory_info(_from, token_id)
     print(f'Promissory info: {promissory_info}')
     # сохраняем данные контракта в виде атрибутов
@@ -77,8 +73,6 @@ def create_metadata(_from, token_id):
         # Запишите метаданные локально
         json.dump(collectible_metadata, f, indent=4)
 
-    # загрузка данных в ipfs(Pinata)
-    print(collectible_metadata)
     # metadata_hash = upload_to_ipfs(collectible_metadata)
     metadata_hash = upload_to_ipfs("./scripts/metadata/")
     metadata_uri = f"<https://ipfs.io/ipfs/{metadata_hash}/>"
@@ -93,58 +87,23 @@ def create_metadata(_from, token_id):
     print('Metadata created success!')
     return metadata_uri
 
-# token_id - id токена, метаданные которого будут обновляться
-# data - массив, где первый элемент адрес holder, а второй дата закрытия
-# Если один из элементов data равен None, то данные этогго атрибута не меняются
-def update_metadata(_from, token_id):
-    print(f'Token id: {token_id}')
-    metadata_filename = f"./scripts/metadata/{token_id}.json"
-    print(f'Metadata file: {metadata_filename}')
-    promissory_info = get_promissory_info(_from, token_id)
-
-    # Чтение и запись файла с обновлёнными таданными адреса и даты закрытия векселя
-    with open(metadata_filename, 'r') as f:
-        json_file = json.load(f)
-    with open(metadata_filename, 'w') as metadata_file:
-        for i in range(9):
-            json_file["attributes"][i]['value'] = str(promissory_info[i]) if type(json_file["attributes"][i]['value']) == type('') else int(promissory_info[i])
-        json.dump(json_file, metadata_file, indent=4)
-
-    #обновляю данные в Pinata
-    hash_ipfs = 'QmeccHuGmE9gXZLdbhKpBhTX8ajrwGszaV8QtRYUYxWtf7'
-    endpoint = "https://api.pinata.cloud/pinning/hashMetadata"
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "authorization": f'Bearer {os.environ.get("PINATA_API_JWT")}'
-    }
-    hash_ipfs_json = {
-        "ipfsPinHash": hash_ipfs,
-        'keyvalues': json.dumps(json_file, indent=2)
-        }
-    response = requests.put(endpoint, headers=headers, json=hash_ipfs_json)
-    print(response.text)
-
 def upload_to_ipfs(data):
     endpoint = "https://api.pinata.cloud/pinning/pinFileToIPFS"
     headers = {
         "accept": "application/json",
-        # "content-type": "application/json",
         "authorization": f'Bearer {os.environ.get("PINATA_API_JWT")}',
     }
-    token_id = data['attributes'][2]['value']
     files = {
         'file': json.dumps(data, indent=4)
     }
     metadata = {
-        'pinataMetadata': json.dumps({"name": data["name"]}, indent=4)
+        'pinataMetadata': json.dumps({"name": data[0]})
     }
     # запрос пин-кода в pinata
     response = requests.post(endpoint, headers=headers, files=files, data=metadata)
     print(f'Response: {response.json()}')
-    returned_hash_IPFS = response.json()['IpfsHash']
     # возвращаем хэш ipfs, где хранятся все нужные данные
-    return returned_hash_IPFS
+    return response.json()['IpfsHash']
 
 # получение инфы о векселе из фабрики
 def get_promissory_info(_from, token_id):
--- scripts/promissory.py
+++ scripts/promissory.py
@@ -6,9 +6,6 @@ def main():
     holder = accounts.load('victor')
     debtor = accounts.load('victor2')
     deploy_promissory(holder, debtor, 10, 200000, 1692126000)
-    set_debtor_consent(debtor)
-    set_holder_consent(holder)
-    pay_promissory(debtor)
 
 def deploy_promissory(_id, _from, _debtor, _promissory_commission, _promissory_amount, _date_of_close):
     promissory_deployed = Promissory.deploy(_id, _from, _debtor, _promissory_commission, _promissory_amount, _date_of_close, {
@@ -17,36 +14,3 @@ def deploy_promissory(_id, _from, _debtor, _promissory_commission, _promissory_a
     })
     print(f'Promissory deployed at {promissory_deployed}')
     return promissory_deployed
-
-# получение соглашения от векселедержателя
-def set_holder_consent(_from):
-    Promissory[-1].setHolderConsent({
-        'from': _from,
-        'priority_fee': '0.2 gwei'
-    })
-    print('Holder consent saved!')
-
-# получение соглашения от векселедателя
-def set_debtor_consent(_from):
-    Promissory[-1].setDebtorConsent({
-        'from':_from,
-        'priority_fee': '0.2 gwei'
-    })
-    print('Debtor consent saved!')
-
-# оплата векселя
-def pay_promissory(_from):
-    Promissory[-1].payPromissory({
-        'from': _from,
-        'value': get_promissory_info(_from)[4],
-        'priority_fee': '2 gwei'
-    })
-    print('Promissory was paid!')
-
-# получение структуры с информацией о векселе через контаркт векселя
-def get_promissory_info(_from):
-    promissory_info = Promissory[-1].getPromissoryInfo({
-        'from': _from
-    })
-    print(f'Promissory info: {promissory_info}')
-    return promissory_info
